31a54d8f20f33a7fdbff184dfc17fce970251f0b
claudio.moscoso@gmail.com <claudio.moscoso@gmail.com@dfe975b6-b2ff-86a4-2ad6-402e05214a33>
2013-02-19 18:05:27 +0000

    se renombra el paquete  a bantotal

---
+++ ibk/trunk/ibk-web/src/cl/buildersoft/ibk/business/service/bantotal/LoginServiceImpl.java
@@ -0,0 +1,39 @@
+package cl.buildersoft.ibk.business.service.bantotal;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.servlet.http.HttpServletRequest;
+
+import cl.buildersoft.framework.database.BSmySQL;
+import cl.buildersoft.framework.exception.BSDataBaseException;
+import cl.buildersoft.framework.util.BSSecurity;
+import cl.buildersoft.framework.util.BSUtils;
+import cl.buildersoft.ibk.business.service.LoginService;
+
+public class LoginServiceImpl implements LoginService {
+
+	@Override
+	public Boolean validate(HttpServletRequest request, String user, String password) {
+		BSmySQL mysql = new BSmySQL();
+		Connection conn = mysql.getConnection(request);
+
+		BSSecurity security = new BSSecurity();
+		password = security.md5(password);
+
+		ResultSet rs = mysql.callSingleSP(conn, "pLogin", BSUtils.array2List(user, password));
+		Boolean out = Boolean.FALSE;
+
+		try {
+			if (rs.next()) {
+				out = Boolean.TRUE;
+			}
+		} catch (SQLException e) {
+			throw new BSDataBaseException(e);
+		}
+
+		return out;
+	}
+
+}
