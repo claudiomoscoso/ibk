ab5a92d5f3187912075018fcaecb7479e26accaf
claudio.moscoso@gmail.com <claudio.moscoso@gmail.com@dfe975b6-b2ff-86a4-2ad6-402e05214a33>
2013-04-12 20:01:00 +0000

    Se complementa el diseño de la base de datos.

--- ibk/trunk/ibk-web/WebContent/WEB-INF/sql/v0.1.0/run-all.sh
+++ ibk/trunk/ibk-web/WebContent/WEB-INF/sql/v0.1.0/run-all.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 if [ -n "$1" ]; then
-	echo 'Borrando tablas... (erase-tables.sql.txt)'
+	echo '>Borrando tablas... (erase-tables.sql.txt)'
 	mysql -D$1 -t -u root -padmin --default-character-set=utf8 < erase-tables.sql.txt
-	echo 'Borrando tablas desestimadas... (erase-deprecated-object.sql.txt)'
+	echo '>Borrando tablas desestimadas... (erase-deprecated-object.sql.txt)'
 	mysql -D$1 -t -u root -padmin --default-character-set=utf8 < erase-deprecated-object.sql.txt
-#	echo 'Creando tablas de plataforma... (create-bsframework.sql.txt)'
+#	echo '>Creando tablas de plataforma... (create-bsframework.sql.txt)'
 #	mysql -D$1 -t -u root -padmin --default-character-set=utf8 < create-bsframework.sql.txt
-	echo 'Creando tablas de sistema...(create-ibk.sql.txt)'
+	echo '>Creando tablas de sistema...(create-ibk.sql.txt)'
 	mysql -D$1 -t -u root -padmin --default-character-set=utf8 < create-ibk.sql.txt
 	
 #	echo 'Creando reglas de plataforma...(rules-ibk.sql.txt)'
@@ -20,7 +20,7 @@ if [ -n "$1" ]; then
 #	echo 'Creando procedimientos de sistema...'
 #	for i in $(ls sp*.sql.txt); do echo "> Running $i..."; mysql -D$1 -u root -padmin < $i; done
 	
-	echo 'Creando datos básicos de plataforma...(data-ibk.sql.txt)'
+	echo '>Creando datos básicos de plataforma...(data-ibk.sql.txt)'
 	mysql -D$1 -t -u root -padmin --default-character-set=utf8 < data-ibk.sql.txt
 	
 #	echo 'Creando datos básicos del sistema...(data-timectrl.sql.txt)'
@@ -29,7 +29,7 @@ if [ -n "$1" ]; then
 #	echo 'Creando opciones de menú...(data-menu.sql.txt)'
 #	mysql -D$1 -t -u root -padmin --default-character-set=utf8 < data-menu.sql.txt
 	
-	echo 'Creando datos de prueba...(data-test.sql.txt)'
+	echo '>Creando datos de prueba...(data-test.sql.txt)'
 	mysql -D$1 -t -u root -padmin --default-character-set=utf8 -t < data-test.sql.txt
 	
 else
