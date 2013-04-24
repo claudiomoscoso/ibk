49b565195106eac10a51af2ad858b022197273d7
claudio.moscoso <claudio.moscoso@gmail.com>
2013-04-24 04:14:04 +0000

    Se incorpora Boostrap

--- ibk/trunk/ibk-web/src/cl/buildersoft/ibk/service/plataform/config/ConfigService.java
+++ ibk/trunk/ibk-web/src/cl/buildersoft/ibk/service/plataform/config/ConfigService.java
@@ -25,7 +25,6 @@ public interface ConfigService {
 
 	public List<PropertyType> listPropertyTypes();
 
-
 	public PropertyType getPropertyType(Long id);
 
 	public List<Option> getOptions(Property property);
